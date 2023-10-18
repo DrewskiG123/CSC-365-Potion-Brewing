53c0d7741fc219d51e0e71e69e6c9dc90dcaed00
Andrew Ji <aeji@calpoly.edu>
2023-10-18 02:06:59 -0700

    fixed barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -26,6 +26,7 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
     r_ml_added = 0
     g_ml_added = 0
     b_ml_added = 0
+    d_ml_added = 0
 
     cost = 0
 
@@ -45,10 +46,14 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
     with db.engine.begin() as connection:
         connection.execute(
             sqlalchemy.text(
-                "UPDATE global_inventory SET num_red_ml = num_red_ml + " + str(r_ml_added) + ",\n"+
-                "   num_green_ml = num_green_ml + " + str(g_ml_added) + ",\n"+
-                "   num_blue_ml = num_blue_ml + " + str(b_ml_added) + ",\n"+
-                "   gold = gold - " + str(cost) + " WHERE id = 0"))
+            """ UPDATE global_inventory 
+                SET num_red_ml = num_red_ml + :r_ml_added,
+                    num_green_ml = num_green_ml + :g_ml_added,
+                    num_blue_ml = num_blue_ml + :b_ml_added,
+                    num_dark_ml = num_dark_ml + :d_ml_added,
+                    gold = gold - :cost 
+                WHERE id = 0
+            """), [{"r_ml_added": r_ml_added, "g_ml_added": g_ml_added, "b_ml_added": b_ml_added, "d_ml_added": d_ml_added, "cost": cost}]) 
 
     return "OK"
 
@@ -67,24 +72,37 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     g_ml_held = -1
     b_potions_held = -1
     b_ml_held = -1
+    d_potions_held = -1
+    d_ml_held = -1
     
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
+        glbl_inv = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
+        ctlg = connection.execute(sqlalchemy.text("SELECT sku, inventory FROM catalog"))
         # fr is the first row of global_inventory
-        fr = result.first()
-        gold_held = fr.gold
-        r_potions_held = fr.num_red_potions
+        fr = glbl_inv.first()
         r_ml_held = fr.num_red_ml
-        g_potions_held = fr.num_green_potions
         g_ml_held = fr.num_green_ml
-        b_potions_held = fr.num_blue_potions
         b_ml_held = fr.num_blue_ml
+        d_ml_held = fr.num_dark_ml
+        gold_held = fr.gold
+
+        for sku, inventory in ctlg:
+            match sku:
+                case "RED_POTION":
+                    r_potions_held = inventory
+                case "GREEN_POTION":
+                    g_potions_held = inventory
+                case "BLUE_POTION":
+                    b_potions_held = inventory
+                case "DARK_POTION":
+                    d_potions_held = inventory
 
     purchase_plan = []
 
     need_r = False
     need_g = False
     need_b = False
+    need_d = False
 
     needed = 0
 
@@ -97,11 +115,25 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     if b_potions_held < 10 and b_ml_held < 1000:
         need_b = True
         needed += 1
+    if d_potions_held < 10 and d_ml_held < 1000:
+        need_d = True
+        needed += 1
 
     if needed > 0: # if some barrels are needed
         gold_per_color = gold_held//needed
         for barrel in wholesale_catalog:
-            if barrel.potion_type[0] == 1 and barrel.price < gold_per_color and barrel.price < gold_held and need_r == True: # if its an affordable red barrel
+            if barrel.price < gold_per_color and barrel.price < gold_held: 
+                if barrel.potion_type[0] == 1 and need_r == True: # if its an affordable red barrel
+                    need_r = False
+                elif barrel.potion_type[1] == 1 and need_g == True: # if its an affordable green barrel
+                    need_g = False
+                elif barrel.potion_type[2] == 1 and need_b == True: # if its an affordable blue barrel
+                    need_b = False
+                elif barrel.potion_type[3] == 1 and need_d == True: # if its an affordable dark barrel
+                    need_d = False
+                else:
+                    continue
+                
                 purchase_plan.append(
                     {
                         "sku":barrel.sku,
@@ -111,27 +143,5 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                         "quantity": 1
                     })
                 gold_held -= barrel.price
-                need_r = False
-            elif barrel.potion_type[1] == 1 and barrel.price < gold_per_color and barrel.price < gold_held and need_g == True: # if its an affordable green barrel
-                purchase_plan.append(
-                    {
-                        "sku":barrel.sku,
-                        "ml_per_barrel": barrel.ml_per_barrel,
-                        "potion_type": barrel.potion_type,
-                        "price": barrel.price,
-                        "quantity": 1
-                    })
-                gold_held -= barrel.price
-                need_g = False
-            elif barrel.potion_type[2] == 1 and barrel.price < gold_per_color and barrel.price < gold_held and need_b == True: # if its an affordable blue barrel
-                purchase_plan.append({
-                        "sku":barrel.sku,
-                        "ml_per_barrel": barrel.ml_per_barrel,
-                        "potion_type": barrel.potion_type,
-                        "price": barrel.price,
-                        "quantity": 1
-                    })
-                gold_held -= barrel.price
-                need_b = False
 
     return purchase_plan
