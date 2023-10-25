57ba86859753c8e1713dd56dc84f4c4e479ac716
Andrew Ji <aeji@calpoly.edu>
2023-10-25 12:41:20 -0700

    updated catalog for ledgers

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -15,13 +15,14 @@ def get_catalog():
     catalog = []
 
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT sku, name, quantity, potion_type, price FROM catalog"))
-        for sku, name, quantity, potion_type, price in result:
-            print(f"{{\n\tname: {name},\n\tsku: {sku},\n\tpotion_type: {potion_type},\n\tquantity: {quantity},\n\tprice: {price}\n}}")
+        result = connection.execute(sqlalchemy.text("SELECT sku, name, potion_type, price FROM catalog"))
+        for sku, name, potion_type, price in result:
+            cur_quant = connection.execute(sqlalchemy.text("SELECT SUM(change) FROM catalog_tracker WHERE sku = :sku"), [{"sku": sku}])
+            print(f"{{\n\tname: {name},\n\tsku: {sku},\n\tpotion_type: {potion_type},\n\tquantity: {cur_quant},\n\tprice: {price}\n}}")
             catalog.append({
                 "sku": sku, 
                 "name": name, 
-                "quantity": quantity, 
+                "quantity": cur_quant,
                 "potion_type": potion_type, 
                 "price": price})
 
