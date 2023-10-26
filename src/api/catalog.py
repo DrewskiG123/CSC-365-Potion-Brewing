05532f76a6a4776275b7ea04bbc641ec0ec967f1
Andrew Ji <aeji@calpoly.edu>
2023-10-26 10:23:51 -0700

    only shows in-stock

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,11 +19,12 @@ def get_catalog():
         for sku, name, quantity, potion_type, price in result:
             # cur_quant = connection.execute(sqlalchemy.text("SELECT SUM(change) FROM catalog_tracker WHERE sku = :sku"), [{"sku": sku}])
             print(f"{{\n\tname: {name},\n\tsku: {sku},\n\tpotion_type: {potion_type},\n\tquantity: {quantity},\n\tprice: {price}\n}}")
-            catalog.append({
-                "sku": sku, 
-                "name": name, 
-                "quantity": quantity,#cur_quant.first()._data[0],
-                "potion_type": potion_type, 
-                "price": price})
+            if quantity > 0:
+                catalog.append({
+                    "sku": sku, 
+                    "name": name, 
+                    "quantity": quantity,#cur_quant.first()._data[0],
+                    "potion_type": potion_type, 
+                    "price": price})
 
     return catalog
