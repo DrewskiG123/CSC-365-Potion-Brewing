c19e23d7a403395d3d6882992f47ddaa24cef23c
Andrew Ji <aeji@calpoly.edu>
2023-11-02 14:08:40 -0700

    reset implemented

--- src/api/admin.py
+++ src/api/admin.py
@@ -17,6 +17,19 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("TRUNCATE TABLE cart_items, carts, catalog, catalog_tracker, global_inventory;"))
+        connection.execute(sqlalchemy.text("""
+            INSERT INTO global_inventory (num_red_ml, num_green_ml, num_blue_ml, num_dark_ml, gold)
+            VALUES (0, 0, 0, 0, 100);"""))
+        connection.execute(sqlalchemy.text("""
+            INSERT INTO catalog (sku, name, price, potion_type, quantity)
+            VALUES
+                ('RED_POTION', 'Health', 50, :r_type, 0),
+                ('GREEN_POTION', 'Stamina', 50, :g_type, 0),
+                ('BLUE_POTION', 'Mana', 60, :b_type, 0),
+                ('DARK_POTION', 'Dark', 65, :d_type, 0);"""), 
+        [{"r_type": [100,0,0,0], "g_type": [0,100,0,0], "b_type": [0,0,100,0], "d_type": [0,0,0,100]}])
     return "OK"
 
 
