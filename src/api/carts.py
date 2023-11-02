a46c65a6c41e6dd744560f589d5edde8089de784
Andrew Ji <aeji@calpoly.edu>
2023-11-02 15:07:13 -0700

    wip

--- src/api/carts.py
+++ src/api/carts.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+from enum import Enum
 
 import sqlalchemy
 from src import database as db
@@ -10,6 +11,62 @@ router = APIRouter(
     tags=["cart"],
     dependencies=[Depends(auth.get_api_key)],
 )
+
+class search_sort_options(str, Enum):
+    customer_name = "customer_name"
+    item_sku = "item_sku"
+    line_item_total = "line_item_total"
+    timestamp = "timestamp"
+
+class search_sort_order(str, Enum):
+    asc = "asc"
+    desc = "desc"   
+
+@router.get("/search/", tags=["search"])
+def search_orders(
+    customer_name: str = "",
+    potion_sku: str = "",
+    search_page: str = "",
+    sort_col: search_sort_options = search_sort_options.timestamp,
+    sort_order: search_sort_order = search_sort_order.desc):
+    """
+    Search for cart line items by customer name and/or potion sku.
+
+    Customer name and potion sku filter to orders that contain the 
+    string (case insensitive). If the filters aren't provided, no
+    filtering occurs on the respective search term.
+
+    Search page is a cursor for pagination. The response to this
+    search endpoint will return previous or next if there is a
+    previous or next page of results available. The token passed
+    in that search response can be passed in the next search request
+    as search page to get that page of results.
+
+    Sort col is which column to sort by and sort order is the direction
+    of the search. They default to searching by timestamp of the order
+    in descending order.
+
+    The response itself contains a previous and next page token (if
+    such pages exist) and the results as an array of line items. Each
+    line item contains the line item id (must be unique), item sku, 
+    customer name, line item total (in gold), and timestamp of the order.
+    Your results must be paginated, the max results you can return at any
+    time is 5 total line items.
+    """
+
+    return {
+        "previous": "",
+        "next": "",
+        "results": [
+            {
+                "line_item_id": 1,
+                "item_sku": "1 oblivion potion",
+                "customer_name": "Scaramouche",
+                "line_item_total": 50,
+                "timestamp": "2021-01-01T00:00:00Z",
+            }
+        ],
+    }
 
 # GOES SOMEWHERE BUT FIGURING OUT WHERE
 # price per ml by color
@@ -66,10 +123,24 @@ def get_cart(cart_id: int):
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ Updates the cart with the desired commoditites """
     with db.engine.begin() as connection:
+        ctlg_stuff = connection.execute(sqlalchemy.text("SELECT id FROM catalog WHERE sku = :item_sku"), [{"item_sku": item_sku}])
+        ctlg_id = ctlg_stuff.first()._data[0]
+        print("catalog id received:", ctlg_id)
         connection.execute(sqlalchemy.text("""
-            INSERT INTO cart_items (cart_id, quantity, catalog_id)
-            SELECT :cart_id, :quantity, catalog.id
-            FROM catalog WHERE catalog.sku = :item_sku"""), [{"cart_id": cart_id, "quantity": cart_item.quantity, "item_sku": item_sku}])
+            IF EXISTS (SELECT quantity FROM cart_items WHERE cart_id = :cart_id AND catalog_id = :ctlg_id) THEN
+                UPDATE cart_items SET quantity = quantity + :quantity
+                WHERE cart_id = :cart_id AND catalog_id = :ctlg_id
+            END IF;
+            ELSE
+              INSERT INTO cart_items (cart_id, quantity, catalog_id)
+              SELECT :cart_id, :quantity, catalog.id
+              FROM catalog WHERE catalog.sku = :item_sku
+        """), [{"cart_id": cart_id, "ctlg_id": ctlg_id, "quantity": cart_item.quantity, "item_sku": item_sku}])
+        # connection.execute(sqlalchemy.text("""
+        #     INSERT INTO cart_items (cart_id, quantity, catalog_id)
+        #     SELECT :cart_id, :quantity, catalog.id
+        #     FROM catalog WHERE catalog.sku = :item_sku
+        # """), [{"cart_id": cart_id, "quantity": cart_item.quantity, "item_sku": item_sku}])
     
     return {"success": True}
         
